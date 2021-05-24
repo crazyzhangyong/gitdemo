0570e5ab98bee77d085be8deda45d274b63dc0d0
Eric Zhang <eric.zhang1@lexisnexis.com>
2021-05-24 13:56:46 +0800

    modified entity Order

--- Order.cs
+++ Order.cs
@@ -2,6 +2,7 @@ public Class Order{
 	public string OrderNo{get;set;}
 	public string Receiver{get;set;}
 	public string MobileNo{get;set;}
+	public decimal TotalAmount{get;set;}
 
 	public Order(string OrderNo,string Receiver){
 		this.OrderNo = OrderNo;
