ef25843e1b7d54c2038423b8905c1be8414c525e
eric.zhang <crazyzhangyong@163.com>
2021-05-24 13:49:29 +0800

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,6 +1,7 @@
 public Class Order{
 	public string OrderNo{get;set;}
 	public string Receiver{get;set;}
+	public string MobileNo{get;set;}
 
 	public Order(string OrderNo,string Receiver){
 		this.OrderNo = OrderNo;
