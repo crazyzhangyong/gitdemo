d5ae07f2a1598794feedfbd669ddd97f3c7ddfa2
Eric Zhang <eric.zhang1@lexisnexis.com>
2021-05-24 10:55:04 +0800

    增加有参构造函数---User.cs

--- User.cs
+++ User.cs
@@ -1,6 +1,10 @@
 public class User{
 	public User(){}
-
+	public User(int Id,string Name){
+		this.Id = Id;
+		this.Name = Name;
+	}
+	
 	public int Id{get;set;}
 	public string Name{get;set;}
 	public string Email{get;set;}
