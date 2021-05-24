5a76a33543bcc72d3b9ee6bb4a0608b9d2422054
eric.zhang <crazyzhangyong@163.com>
2021-05-24 13:10:52 +0800

    Create Order.cs

--- Order.cs
+++ Order.cs
@@ -1,4 +1,9 @@
 public Class Order{
 	public string OrderNo{get;set;}
 	public string Receiver{get;set;}
+	
+	public Order(string orderNo,string receiver){
+		this.OrderNo = orderNo;
+		this.Receiver = receiver;
+	}
 }
