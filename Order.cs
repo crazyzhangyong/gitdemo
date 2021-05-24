2be1d83bed97cacb73ed948787ad75766d098656
Eric Zhang <eric.zhang1@lexisnexis.com>
2021-05-24 11:15:02 +0800

    added parameterized constructor for entity Order

--- Order.cs
+++ Order.cs
@@ -1,4 +1,9 @@
 public Class Order{
 	public string OrderNo{get;set;}
 	public string Receiver{get;set;}
+
+	public Order(string OrderNo,string Receiver){
+		this.OrderNo = OrderNo;
+		this.Receiver = Receiver;
+	}
 }
